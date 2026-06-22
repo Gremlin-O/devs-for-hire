4250a99badc3978ca13054c1c5e1adae67c3fbae
ivan <ivandacenko286@gmail.com>
2026-06-22 18:50:42 +0300

    added yandex metrica

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,12 @@
   import App from "./app/App.tsx";
   import "./styles/index.css";
 
+  const YM_ID = 110067332;
+
+  window.addEventListener("hashchange", () => {
+    const ym = (window as Window & { ym?: (...args: unknown[]) => void }).ym;
+    ym?.(YM_ID, "hit", window.location.href);
+  });
+
   createRoot(document.getElementById("root")!).render(<App />);
   
